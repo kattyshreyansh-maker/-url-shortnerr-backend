5c499e6fd890cef45c05e27b6e2318ee7ea8f238
kattyshreyansh-maker <kattyshreyansh@gmail.com>
2026-05-31 23:36:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const Url = require('./models/Url');
+const Url = require('./models/url');
 
 const app = express();
 
